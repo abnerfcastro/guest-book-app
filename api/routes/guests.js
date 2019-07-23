a9bac23cf2b51bad0926a608a0f3e74268f3f51c
Abner Castro <abnerfcastro@gmail.com>
2019-07-23 16:37:46 -0300

    Add model and CRUD routes for Guest API

--- api/routes/guests.js
+++ api/routes/guests.js
@@ -1,8 +1,86 @@
 const express = require('express');
 const router = express.Router();
 
+const Guest = require('../models/guests');
+
 router.get('/', (req, res, next) => {
-    res.status(200).send('Not implemented yet.');
+    Guest.find()
+        .exec()
+        .then(docs => res.status(200).json(docs))
+        .catch(err => {
+            res.status(500).json({
+                error: err
+            });
+        });
+});
+
+router.get('/:guestId', (req, res, next) => {
+    const id = req.params.guestId;
+    Guest.findById(id)
+        .exec()
+        .then(doc => {
+            if (doc) {
+                res.status(200).json(doc);
+            } else {
+                res.status(404).json({ message: "No valid entry found for provided ID" });
+            }
+        })
+        .catch(err => {
+            res.status(500).json({ error: err });
+        });
+});
+
+router.post('/', (req, res, next) => {
+    const { firstname, lastname, date, country } = req.body;
+    const guest = new Guest({ firstname, lastname, date, country });
+
+    guest
+        .save()
+        .then(result => {
+            res.status(201).json({
+                message: 'Handling POST requests to /guests',
+                createdObject: result
+            });
+        })
+        .catch(err => {
+            res.status(500).json({
+                error: err
+            });
+        });
+});
+
+router.patch("/:guestId", (req, res, next) => {
+    const id = req.params.guestId;
+    const updateOps = {};
+
+    for (const key of Object.keys(req.body)) {
+        updateOps[key] = req.body[key];
+    }
+
+    Guest.update({ _id: id }, { $set: updateOps }, { runValidators: true })
+        .exec()
+        .then(result => {
+            res.status(200).json(result);
+        })
+        .catch(err => {
+            res.status(500).json({
+                error: err
+            });
+        });
+});
+
+router.delete("/:guestId", (req, res, next) => {
+    const id = req.params.productId;
+    Guest.remove({ _id: id })
+        .exec()
+        .then(result => {
+            res.status(200).json(result);
+        })
+        .catch(err => {
+            res.status(500).json({
+                error: err
+            });
+        });
 });
 
 module.exports = router;
